f7e1de8f705f0d17ee25e5200eb1e52539c369fd
Jayleh <jaylehyun@gmail.com>
2018-07-30 15:46:49 -0700

    update contact view

--- justin/forms.py
+++ justin/forms.py
@@ -4,10 +4,10 @@ from wtforms.validators import InputRequired, Email, Length
 
 
 class MessageForm(FlaskForm):
-    name = StringField("Name", validators=[InputRequired(), Length(min=2, max=50)])
-    email = StringField("Email", validators=[InputRequired(), Email()])
+    name = StringField("Your Name", validators=[InputRequired(), Length(min=2, max=50)])
+    email = StringField("Your Email", validators=[InputRequired(), Email()])
     subject = StringField("Subject", validators=[Length(max=100)])
-    message = TextAreaField("Message", validators=[InputRequired(), Length(min=2)])
+    message = TextAreaField("Your Message", validators=[InputRequired(), Length(min=2)])
     connection = SelectField("How did you find me?")
     recaptcha = RecaptchaField()
     submit = SubmitField("Send")
--- justin/static/js/contact.js
+++ justin/static/js/contact.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('textarea#textarea1').characterCounter();
+    $('textarea#message').characterCounter();
 
     $('select').formSelect();
 
