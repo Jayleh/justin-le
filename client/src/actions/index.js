c139ce40392db3cbe0a6ab0e58ba3a1873646ce0
Jayleh <jaylehyun@gmail.com>
2019-01-12 17:53:28 -0800

    commenting out console log of form values

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ import server from '../apis/server';
 import { SEND_MESSAGE } from './types';
 
 export const submitContact = formValues => async dispatch => {
-  console.log(formValues);
+  // console.log(formValues);
   await server.post('/api/email', formValues);
 
   dispatch({ type: SEND_MESSAGE });
