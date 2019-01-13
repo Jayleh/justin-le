49d353dfffcdfc62e872954b8211ae60d7ba850b
Jayleh <jaylehyun@gmail.com>
2019-01-12 16:30:10 -0800

    updating production keys

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
   googleUsername: process.env.GOOGLE_USERNAME,
   googlePassword: process.env.GOOGLE_PASSWORD,
   googleClientId: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  refreshToken: process.env.REFRESH_TOKEN,
+  defaultRecipient: process.env.DEFAULT_RECIPIENT
 };
