dde401438c6d58a7efc377dae53fed3e03e97ff1
Jayleh <jaylehyun@gmail.com>
2019-01-13 18:27:02 -0800

    deleting unused keys

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,4 @@
 module.exports = {
-  googleUsername: process.env.GOOGLE_USERNAME,
-  googlePassword: process.env.GOOGLE_PASSWORD,
-  googleClientId: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  refreshToken: process.env.REFRESH_TOKEN,
   defaultRecipient: process.env.DEFAULT_RECIPIENT,
-  etherealUsername: process.env.ETHEREAL_USERNAME,
-  etherealPassword: process.env.ETHEREAL_PASSWORD,
   sendGridKey: process.env.SEND_GRID_KEY
 };
